355a83ceef6456c9a47be608a1538011365933da
msceada1 <195111856+msceada1@users.noreply.github.com>
2026-03-19 13:40:24 +0100

    cambio a java NIO

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Ejercicio1 {
 
@@ -10,13 +12,17 @@ public class Ejercicio1 {
 
         try (FileReader fr = new FileReader(f);
              BufferedReader br = new BufferedReader(fr)) {
-            int contador = 0;
+            /*int contador = 0;
 
             while (br.readLine() != null) {
                 contador++;
             }
 
             System.out.printf("El fichero leeme.txt tiene %d lineas", contador);
+             */
+
+            Path fichero = Path.of("./src/leeme.txt");
+            System.out.printf("El fichero leeme.txt tiene %d lineas", Files.lines(fichero).count());
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
