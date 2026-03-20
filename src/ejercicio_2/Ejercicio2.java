6525940de623b033ca8499cdfd740ddc235b5737
msceada1 <195111856+msceada1@users.noreply.github.com>
2026-03-20 09:01:41 +0100

    nueva solucion

--- src/ejercicio_2/Ejercicio2.java
+++ src/ejercicio_2/Ejercicio2.java
@@ -17,3 +17,26 @@ public class Ejercicio2 {
         }
     }
 }
+
+/*
+package Buffer.Ejercicio2;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Ejercicio2 {
+    static void main() {
+        Path p = Path.of("./src/Buffer/Ejercicio1/leeme.txt");
+
+        try(Stream<String> lineas = Files.lines(p)) {
+            System.out.println(lineas.collect(Collectors.joining(" ")));
+        }catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
+ */
