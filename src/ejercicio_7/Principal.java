aa777051d28ff0e6d573f28c6a7e96a76caccee7
msceada1 <195111856+msceada1@users.noreply.github.com.>
2026-03-24 22:46:16 +0100

    nuevo ejercicio

---
+++ src/ejercicio_7/Principal.java
@@ -0,0 +1,11 @@
+package ejercicio_7;
+
+import utils.MiEntradaSalida;
+
+public class Principal {
+
+    private static void listarDirectorio() {
+        String nombre = MiEntradaSalida.leerCadena("Indica el nombre del directorio: ");
+
+    }
+}
