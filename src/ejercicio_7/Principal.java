d0debce3314d767114650d35b7e88367b57caef5
msceada1 <195111856+msceada1@users.noreply.github.com.>
2026-03-26 09:54:42 +0100

    avance

--- src/ejercicio_7/Principal.java
+++ src/ejercicio_7/Principal.java
@@ -2,14 +2,95 @@ package ejercicio_7;
 
 import utils.MiEntradaSalida;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.stream.Stream;
 
 public class Principal {
 
-    private static void listarDirectorio() {
+    public static void listarDirectorioQueEmpiezaPor() {
         String nombre = MiEntradaSalida.leerCadena("Indica el nombre del directorio: ");
         Path directorio = Path.of("./src/ejercicio_7", nombre);
+        String cadena = MiEntradaSalida.leerCadena("¿Por que palabra o letra empieza?");
 
+        if (Files.isDirectory(directorio)) {
+            try (Stream<Path> ficheros = Files.list(directorio)) {
+                ficheros.filter(p -> p.getFileName().toString().startsWith(cadena))
+                        .forEach(p -> {
+                            if (Files.isDirectory(p)) {
+                                System.out.printf("%s - directorio %n", p.getFileName());
+                            } else {
+                                try {
+                                    System.out.printf("%s %.2f kb %n", p.getFileName(), Files.size(p) / 1024.0);
+                                } catch (IOException e) {
+                                    System.err.println(e.getMessage());
+                                }
+                            }
+                        });
+            } catch (IOException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+    }
 
+    public static void buscarArchivosPorExtension() {
+        String nombreDirectorio = MiEntradaSalida.leerCadena("Indica el nombre del directorio:\n");
+        Path directorio = Path.of("src", "ejercicio_7", nombreDirectorio);
+        String extension = MiEntradaSalida.leerCadena("Indica la extension:\n");
+
+        if (Files.isDirectory(directorio)) {
+            try (Stream<Path> ficheros = Files.list(directorio)) {
+                ficheros.filter(p -> p.getFileName().toString().endsWith("." + extension))
+                        .forEach(p -> {
+                            if (Files.isDirectory(p)) {
+                                System.out.printf("%s - directorio %n", p.getFileName());
+                            } else {
+                                try {
+                                    System.out.printf("%s %.2f kb %n", p.getFileName(), Files.size(p) / 1024.0);
+                                } catch (IOException e) {
+                                    System.err.println(e.getMessage());
+                                }
+                            }
+                        });
+            } catch (IOException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+    }
+
+    public static void buscarArchivoEnDirectorio() {
+        String nombreDirectorio = MiEntradaSalida.leerCadena("Indica el nombre del directorio:\n");
+        Path directorio = Path.of("src", nombreDirectorio);
+        String nombreFichero = MiEntradaSalida.leerCadena("Indica el nombre del fichero");
+
+        if (Files.isDirectory(directorio)) {
+            try (Stream<Path> ficheros = Files.list(directorio)) {
+                ficheros.filter(p -> p.getFileName().toString().equalsIgnoreCase(nombreFichero))
+                        .forEach(p -> {
+                            if (!Files.isDirectory(p)) {
+                                try {
+                                    System.out.println("Informacion fichero " + p.getFileName().toString() + "" +
+                                            "ruta: " + p.toAbsolutePath() + "\npeso en kb: " + Files.size(p));
+                                } catch (IOException e) {
+                                    System.err.println(e.getMessage());
+                                }
+                            }
+                        });
+            } catch (IOException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+    }
+
+    public static void buscarArchivoEnDirectorioRecursiva() {
+        String nombreDirectorio = MiEntradaSalida.leerCadena("Indica el nombre del directorio:\n ");
+        Path directorio = Path.of("src", nombreDirectorio);
+        String nombreFichero = MiEntradaSalida.leerCadena("Indica el nombre del fichero:\n");
+
+        if (Files.isDirectory(directorio)) {
+
+        }
     }
 }
