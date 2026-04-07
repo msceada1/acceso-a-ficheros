5d20bd267cc9791daaba856458298e683c595bb1
msceada1 <195111856+msceada1@users.noreply.github.com.>
2026-04-07 20:36:41 +0200

    cambios

--- src/ejercicio_11/Principal.java
+++ src/ejercicio_11/Principal.java
@@ -1,10 +1,12 @@
 package ejercicio_11;
 
+import java.nio.file.Path;
+
 public class Principal {
 
     public static void main(String[] args) {
 
+        Path fichero = Path.of("src", "ejercicio_11", "nombres.txt");
+        String patron = "^[A-ZÁÉÍÓÚÑ]+\\s[A-ZÁÉÍÓÚÑ]+\\s[A-ZÁÉÍÓÚÑ]+\\s[1-4][A-Z]$";
     }
-
-
 }
